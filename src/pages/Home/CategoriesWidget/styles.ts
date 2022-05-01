3d3d064dfc8eb08e06c8e88086b1ecc4187d93df
Michael <michael@Michaels-MacBook-Pro.local>
2022-05-01 22:05:40 +0700

    style(dsc): categories widget

--- src/pages/Home/CategoriesWidget/styles.ts
+++ src/pages/Home/CategoriesWidget/styles.ts
@@ -1,11 +1,23 @@
 import { css } from "@emotion/css";
+import { mq } from "@styles/mixins";
+import { MD } from "@styles/variables/breakpoints";
 
 export const styBookCategories = css`
+  ${mq(MD)} {
+    padding: 32px;
+  }
+
   .list {
     display: flex;
     flex-wrap: wrap;
     margin-top: 16px;
     column-gap: 8px;
     row-gap: 8px;
+
+    ${mq(MD)} {
+      margin-top: 24px;
+      column-gap: 16px;
+      row-gap: 15px;
+    }
   }
 `;
